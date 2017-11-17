85b06dfdc1660206a6c8cad0afb5cdb38964cfa1
Joshua Kleckner <lokua@lokua.net>
2017-11-17 00:50:41 -0600

    wat?

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
  * @name hello-wavepot
  * @author Lokua
  */
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
 
-const TAU = Math.PI * 2
-const sin = (frequency, time) => Math.sin(time * TAU * frequency)
+const tau = Math.PI * 2
+const sin = (f, t) => Math.sin(t * tau * f)
+const rect = compose(x => x < 0 ? -1 : 1, sin)
 
 export function dsp(t) {
-  const sample = sin(60, t)
+  const sample = rect(60, sin(0.1, t))
   const gain = 0.25
   
   return sample * gain
