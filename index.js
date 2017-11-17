e5b18677e05c6d44afc722150303d2fdf902ea87
Joshua Kleckner <lokua@lokua.net>
2017-11-17 01:23:46 -0600

    getting somewhere, finally

--- index.js
+++ index.js
@@ -7,18 +7,25 @@ const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
 
 const tau = Math.PI * 2
 const sin = (f, t) => Math.sin(t * tau * f)
-const rect = compose(x => x < 0 ? -1 : 1, sin)
 const square = x => x < 0 ? -1 : 1
+const rect = compose(square, sin)
 const sum = (...x) => x.reduce((t, v) => v + t, 0)
 const avg = (...x) => sum(...x) / x.length
 const clip = x => x < -1 ? -1 : x > 1 ? 1 : x
+const mtof = m => Math.pow(2, (m - 69) / 12) * 440
+const ftom = f => Math.round((12 * (Math.log(f / 440) / Math.log(2))) + 69)
 
 export function dsp(t) {
   const gain = 0.5
   let sample
   
-  sample = sin(60, sin(0.2, t))
+  sample = sin(mtof(12), sin(0.01, t))
   sample = avg(sample, square(sample)) * gain
   
-  return (sin(40, t) + sin(43, t) + sin(100, t)) / 3
+  return (
+    sin(mtof(7), sample) + 
+    (sin(mtof(40), t) * Math.abs(sin(0.5, t))) + 
+    (sin(mtof(47), t) * Math.abs(sin(0.3, t / 5))) +
+    (sin(mtof(51), t) * Math.abs(sin(0.1, t / 3)))
+  ) / 4
 }
