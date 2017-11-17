49c29ccd418d2b11ebdd982edfe4c615f42ccfd1
Joshua Kleckner <lokua@lokua.net>
2017-11-17 01:05:39 -0600

    ¯\_(ツ)_/¯

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
 const tau = Math.PI * 2
 const sin = (f, t) => Math.sin(t * tau * f)
 const rect = compose(x => x < 0 ? -1 : 1, sin)
+const square = x => x < 0 ? -1 : 1
+const sum = (...x) => x.reduce((t, v) => v + t, 0)
+const avg = (...x) => sum(...x) / x.length
+const clip = x => x < -1 ? -1 : x > 1 ? 1 : x
 
 export function dsp(t) {
-  const sample = rect(60, sin(0.1, t))
-  const gain = 0.25
+  const gain = 0.5
+  let sample
   
-  return sample * gain
+  sample = sin(60, sin(0.2, t))
+  sample = avg(sample, square(sample)) * gain
+  
+  return (sin(40, t) + sin(43, t) + sin(100, t)) / 3
 }
